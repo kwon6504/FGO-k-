837a2121947c26ffb9f4411af2dff98d2175691c
kwon <kwon.com>
2024-03-21 10:16:42 +0900

    [FGO 도감] -0.01 api 출력 및 캐릭터 , 스킬 아이콘 출력 확인

---
+++ src/main/java/com/peisia/controller/BoardController.java
@@ -0,0 +1,21 @@
+package com.peisia.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+@RequestMapping("/board/*")
+@AllArgsConstructor
+@Controller
+public class BoardController {
+//	private BoardService service;
+	
+//	@GetMapping("/getlist")
+//	public String getList() {
+//		
+//	}
+}
